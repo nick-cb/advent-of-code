6fa25342f8b404528d632597db2c0bb7bd328708
nick-cb <51416907+nick-cb@users.noreply.github.com>
2023-05-20 17:33:45 +0000

    day1 - part1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::fs;
+
+pub fn process_part1(input: &str) -> String {
+    let result = input.split("\n\n").map(|elf_load| {
+        elf_load.lines().map(|item| {
+            item.parse::<u32>().unwrap()
+        }).sum::<u32>()
+    }).max().unwrap();
+
+    return result.to_string();
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", std::env::current_dir().unwrap().display());
+
+    let contents = fs::read_to_string("input.txt").unwrap();
+    println!("{}", process_part1(contents.as_str()));
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let input = "1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+        assert_eq!(process_part1(input), "24000");
+    }
 }
