9d05cf516e6b024fad9cb088b46b00c3d03b9ad0
nick-cb <51416907+nick-cb@users.noreply.github.com>
2023-05-21 04:17:43 +0000

    organize

--- 2022/rust/src/day1/mod.rs
+++ 2022/rust/src/day1/mod.rs
@@ -1,12 +1,14 @@
-mod day1;
 use std::fs;
 use crate::day1::part1::process_part1;
 use crate::day1::part2::process_part2;
 
-fn main() {
+pub mod part1;
+pub mod part2;
+
+pub fn run() {
     println!("{}", std::env::current_dir().unwrap().display());
 
-    let contents = fs::read_to_string("input.txt").unwrap();
+    let contents = fs::read_to_string("../input.txt").unwrap();
     println!("{}", process_part1(contents.as_str()));
     println!("{}", process_part2(contents.as_str()));
 }
---
+++ 2022/rust/src/main.rs
@@ -0,0 +1,6 @@
+mod day1;
+
+fn main() {
+    println!("hello AOC!");
+    // day1::run();
+}
