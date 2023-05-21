19eb60a8566c480fe516653d942d88a45502fa9c
nick-cb <51416907+nick-cb@users.noreply.github.com>
2023-05-21 14:49:00 +0000

    update

--- 2022/rust/src/day2/mod.rs
+++ 2022/rust/src/day2/mod.rs
@@ -5,5 +5,6 @@ mod part2;
 
 pub fn run() {
     let input = fs::read_to_string("src/day2/input.txt").unwrap();
-    println!("Part1: {}", part1::run(input.as_str()));
+    println!("Part 1: {}", part1::run(input.as_str()));
+    println!("Part 2: {}", part2::run(input.as_str()));
 }
