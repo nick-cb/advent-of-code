f7cf53de58b082e6917f1b741f8778fbf9c7a7e8
nick-cb <51416907+nick-cb@users.noreply.github.com>
2023-05-21 03:40:32 +0000

    day1 - part2

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,25 @@ pub fn process_part1(input: &str) -> String {
     return result.to_string();
 }
 
+pub fn process_part2(input: &str) -> String {
+    let mut result = input.split("\n\n").map(|elf_load| {
+        elf_load.lines().map(|item| {
+            item.parse::<u32>().unwrap()
+        }).sum::<u32>()
+    }).collect::<Vec<_>>();
+
+    result.sort_by(|a, b| b.cmp(a));
+    let sum: u32 = result.iter().take(3).sum();
+
+    sum.to_string()
+}
+
 fn main() {
     println!("{}", std::env::current_dir().unwrap().display());
 
     let contents = fs::read_to_string("input.txt").unwrap();
     println!("{}", process_part1(contents.as_str()));
+    println!("{}", process_part2(contents.as_str()));
 }
 
 #[cfg(test)]
@@ -38,5 +52,6 @@ mod test {
 
 10000";
         assert_eq!(process_part1(input), "24000");
+        assert_eq!(process_part2(input), "45000");
     }
 }
