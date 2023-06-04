07c5060874614057e05ddd765cfa08aea547d38b
nick-cb <51416907+nick-cb@users.noreply.github.com>
2023-06-04 03:12:59 +0000

    day6 - part2

--- 2022/rust/src/day6/mod.rs
+++ 2022/rust/src/day6/mod.rs
@@ -1,9 +1,11 @@
 use std::fs;
 
 mod part1;
+mod part2;
 
 pub fn run() {
     let input = fs::read_to_string("src/day6/input.txt").unwrap();
     println!("Part 1 - Brute force: {}", part1::brute_force(input.as_str()));
     println!("Part 1 - Using windows: {}", part1::using_windows(input.as_str()));
+    println!("Part 2 - Using windows: {}", part2::run(input.as_str()));
 }
---
+++ 2022/rust/src/day6/part2.rs
@@ -0,0 +1,30 @@
+use std::collections::BTreeSet;
+
+pub fn run(input: &str) -> String {
+    let chars = input.chars().collect::<Vec<char>>();
+    let (i, _) = chars.windows(14).enumerate().find(|(_i, slice)| {
+        let set = slice.iter().collect::<BTreeSet<&char>>();
+        slice.len() == set.len()
+    }).unwrap();
+
+    (i + 1 + 13).to_string()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    const INPUT1: &str = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
+    const INPUT2: &str = "bvwbjplbgvbhsrlpgdmjqwftvncz";
+    const INPUT3: &str = "nppdvjthqldpwncqszvftbrmjlhg";
+    const INPUT4: &str = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg";
+    const INPUT5: &str = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw";
+
+    #[test]
+    fn it_works() {
+        assert_eq!(run(INPUT1), "19");
+        assert_eq!(run(INPUT2), "23");
+        assert_eq!(run(INPUT3), "23");
+        assert_eq!(run(INPUT4), "29");
+        assert_eq!(run(INPUT5), "26");
+    }
+}
