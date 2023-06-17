aa71c6eb9d09e0b852561899d2886a4b2bede74d
18DH110753 <51535584+18DH110753@users.noreply.github.com>
2023-06-17 02:54:20 +0000

    day8 - part1 - brute force

--- 2022/rust/src/day8/mod.rs
+++ 2022/rust/src/day8/mod.rs
@@ -4,5 +4,5 @@ mod part1;
 
 pub fn run() {
     let input = fs::read_to_string("src/day8/input.txt").unwrap();
-
+    println!("Part1 - Brute force: {}", part1::run(&input));
 }
--- 2022/rust/src/day8/part1.rs
+++ 2022/rust/src/day8/part1.rs
@@ -3,33 +3,41 @@ pub fn run(input: &str) -> String {
     let rows = &lines.iter().map(|line| {
         line.chars().map(|c| c as u32 - '0' as u32).collect::<Vec<u32>>()
     }).collect::<Vec<Vec<u32>>>();
+
     let result = rows.iter().enumerate().map(|(i, row)| {
         row.iter().enumerate().filter(|(j, current) | {
-            if i == 0 || j.clone() == rows.len() - 1 {
+            if i == 0 || i.clone() == rows.len() - 1 {
                 return true;
             }
+            if *j == 0 || *j == row.len() - 1 {
+                return true;
+            }
+            let mut left = true;
+            let mut top = true;
+            let mut bottom = true;
+            let mut right = true;
             for _j in 0..j.clone() {
                 if rows[i.clone()][_j] >= **current {
-                    return false;
+                    left = false;
                 }
             }
 
-            for j_ in j.clone()..rows[i].len() {
+            for j_ in (j.clone() + 1)..rows[i].len() {
                 if  rows[i.clone()][j_] >= **current {
-                    return false;
+                    right = false;
                 }
             }
             for _i in 0..i {
                 if rows[_i][j.clone()] >= **current {
-                    return false;
+                    top = false;
                 }
             }
-            for i_ in i..rows.len() {
+            for i_ in (i + 1)..rows.len() {
                 if rows[i_][j.clone()] >= **current {
-                    return false;
+                    bottom = false;
                 }
             }
-            true
+            left || right || top || bottom
         }).map(|(_, val)| val).collect::<Vec<&u32>>().len()
     }).sum::<usize>();
 
