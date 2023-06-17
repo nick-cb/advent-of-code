8d2ce1a923328671914039741d6944b39bfe3ae6
nick-cb <51416907+nick-cb@users.noreply.github.com>
2023-06-17 02:25:22 +0000

    Pending changes exported from your codespace

--- 2022/rust/src/day8/part1.rs
+++ 2022/rust/src/day8/part1.rs
@@ -1,28 +1,39 @@
 pub fn run(input: &str) -> String {
     let lines = input.split("\n").collect::<Vec<&str>>();
     let rows = &lines.iter().map(|line| {
-        line.chars().map(|c| c.parse::<u32>()).collect::<Vec<u32>>()
-    }).collect::<Vec<Vec<u32>>();
-
-
-    let total_visible: u32 = 0;
-    for i in 0..rows.len() {
-        if i == 0 || i == &rows.len() {
-            total_visible += &rows[i].len();
-            continue;
-        }
-        total_visible += 2;
-        for j in 1..rows[i].len() - 1 {
-            let current = rows[i][j];
-            if j == 0 || j == rows[i].len() - 1 {
-                continue;
+        line.chars().map(|c| c as u32 - '0' as u32).collect::<Vec<u32>>()
+    }).collect::<Vec<Vec<u32>>>();
+    let result = rows.iter().enumerate().map(|(i, row)| {
+        row.iter().enumerate().filter(|(j, current) | {
+            if i == 0 || j.clone() == rows.len() - 1 {
+                return true;
             }
-            if rows[i][j -1] < current || rows[i][j + 1] < current || rows[i -1][j] < current || row[i + 1][j] < current {
-                total_visible += 1;
+            for _j in 0..j.clone() {
+                if rows[i.clone()][_j] >= **current {
+                    return false;
+                }
             }
-        }
-    }
-    "nothing".to_string()
+
+            for j_ in j.clone()..rows[i].len() {
+                if  rows[i.clone()][j_] >= **current {
+                    return false;
+                }
+            }
+            for _i in 0..i {
+                if rows[_i][j.clone()] >= **current {
+                    return false;
+                }
+            }
+            for i_ in i..rows.len() {
+                if rows[i_][j.clone()] >= **current {
+                    return false;
+                }
+            }
+            true
+        }).map(|(_, val)| val).collect::<Vec<&u32>>().len()
+    }).sum::<usize>();
+
+    result.to_string()
 }
 
 #[cfg(test)]
@@ -36,6 +47,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert_eq!(run(INPUT), "1");
+        assert_eq!(run(INPUT), "21");
     }
 }
