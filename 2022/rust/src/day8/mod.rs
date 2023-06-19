9d343a94b20536002fc019eb180c3d4743cc4a19
18DH110753 <51535584+18DH110753@users.noreply.github.com>
2023-06-19 14:13:42 +0000

    day8 - part2 - brute force

--- 2022/rust/src/day8/mod.rs
+++ 2022/rust/src/day8/mod.rs
@@ -1,8 +1,10 @@
 use std::fs;
 
 mod part1;
+mod part2;
 
 pub fn run() {
     let input = fs::read_to_string("src/day8/input.txt").unwrap();
     println!("Part1 - Brute force: {}", part1::run(&input));
+    println!("Part2 - Brute force: {}", part2::run(&input));
 }
