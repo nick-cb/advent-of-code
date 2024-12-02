207cc61b82bdb9d0e35dc95f490c4ca310c86ce3
VI Nguyen <nguyenhungvicb@gmail.com>
2024-12-02 20:09:19 +0700

    2024/golang: day1

--- 2024/golang/day1/main.go
+++ 2024/golang/day1/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -21,7 +22,7 @@ func main() {
 	scanner := bufio.NewScanner(bytes.NewReader(data))
 	for scanner.Scan() {
 		line := scanner.Text()
-		sides := strings.Split(line, " ")
+		sides := strings.Split(line, "   ")
 
 		left, err := strconv.Atoi(sides[0])
 		if err != nil {
@@ -29,11 +30,21 @@ func main() {
 		}
 		leftSide = append(leftSide, left)
 
-		right, err := strconv.Atoi(sides[0])
+		right, err := strconv.Atoi(sides[1])
 		if err != nil {
 			log.Fatal(err)
 		}
 		rightSide = append(rightSide, right)
 	}
+	fmt.Printf("%v", rightSide)
 
+	slices.Sort(leftSide)
+	slices.Sort(rightSide)
+	var total = 0
+	for i, leftValue := range leftSide {
+		fmt.Printf("%d: %d - %d\n", i, leftValue, rightSide[i])
+		total += (max(leftValue, rightSide[i]) - min(leftValue, rightSide[i]))
+	}
+
+	fmt.Printf("%d", total)
 }
